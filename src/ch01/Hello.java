295203b4fb0ca817aba6aa2919affd5e2d9b3423
jt <1qkrtjsgml1@naver.com>
2024-01-23 11:53:10 +0900

    득개의 과일이 수경딘었습니다.

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hamo is Hello");
         System.out.println("Hamo is God");
-        System.out.println("My Master Hamo");
+        System.out.println("My Master Hamo.");
     }
 }
--- src/ch01/World.java
+++ src/ch01/World.java
@@ -4,6 +4,6 @@ public class World {
     public static void main(String[] args) {
 
         System.out.println("Hamo is World");
-        System.out.println("뜨ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ악");
+        System.out.println("뜨ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ악");
     }
 }
