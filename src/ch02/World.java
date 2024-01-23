9658800ae8c72c553a5e412610c88621a048a598
jt <1qkrtjsgml1@naver.com>
2024-01-23 12:46:07 +0900

    PULL 된 창을 다시 PUSH 합니다.

--- src/ch02/World.java
+++ src/ch02/World.java
@@ -2,6 +2,8 @@ package ch02;
 
 public class World {
     public static void main(String[] args) {
+
         System.out.println("아 이거 뭔가요!!!!");
+        System.out.println("PULL TEST01");
     }
 }
