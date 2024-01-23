8ec709bd9d583eb19514e81c7df8091b3e8cb1cf
jt <1qkrtjsgml1@naver.com>
2024-01-23 11:52:25 +0900

    HELLO CLASS 수정하기

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hamo is Hello");
         System.out.println("Hamo is God");
+        System.out.println("My Master Hamo");
     }
 }
--- src/ch01/World.java
+++ src/ch01/World.java
@@ -2,6 +2,8 @@ package ch01;
 
 public class World {
     public static void main(String[] args) {
+
         System.out.println("Hamo is World");
+        System.out.println("뜨ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ악");
     }
 }
